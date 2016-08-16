2119d79247bce033448ee50713631d8abaacc2bb
HarrisZhang <hc_zhang@Ctrip.com>
2016-08-16 18:18:27 +0800

    update

--- Ranta.ReactjsDemo/Scripts/jsx/Grid.jsx
+++ Ranta.ReactjsDemo/Scripts/jsx/Grid.jsx
@@ -8,21 +8,14 @@ var Line = React.createClass({
         return (<tr><Cell text={this.props.index } /><Cell text={this.props.item.name } /><Cell text={this.props.item.department } /><Cell text={this.props.item.phone } /></tr>)
     }
 });
-var Rows = React.createClass({
-    render: function () {
-        var rows = this.props.items.map(function (rowData, index) {
-            return <Line key={index} index={index} item={rowData } />;
-        });
-
-        return (<tbody>{rows}</tbody>);
-    }
-});
 var Table = React.createClass({
     render: function () {
         var head = <thead><tr><td>#</td><td>Name</td><td>Department</td><td>Phone</td></tr></thead>;
 
-        var body = <Rows items={this.props.items} />;
+        var rows = this.props.items.map(function (rowData, index) {
+            return <Line key={index} index={index} item={rowData } />;
+        });
 
-        return <table className="table table-bordered table-striped">{head}{body}</table>;
+        return <table className="table table-bordered table-striped">{head}<tbody>{rows}</tbody></table>;
     }
 });
